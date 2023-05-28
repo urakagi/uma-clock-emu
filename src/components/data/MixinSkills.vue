9ac487829bbc0c612bb9fc8467e16fd372cf5676
Romulus Urakagi Tsai <urakagi@gmail.com>
2023-05-28 09:02:22 +0800

    Add tooltip localization

--- src/components/data/MixinSkills.vue
+++ src/components/data/MixinSkills.vue
@@ -40,13 +40,7 @@ export default {
       }
       for (const skillWrapper of origin) {
         for (const variant of skillWrapper.variants) {
-          // i18n name
-          const jaName = variant.name;
-          const localName = this.$t(`skill.${jaName}`);
-          if (localName.startsWith('skill.')) {
-            newSkillNames[jaName] = '';
-          }
-          variant.name = localName && !localName.startsWith('skill.') ? localName : jaName;
+          this.localizeSkill(variant, skillWrapper, newSkillNames);
         }
       }
       if (Object.keys(newSkillNames).length > 0) {
@@ -65,13 +59,7 @@ export default {
         });
       }
       for (const skill of origin) {
-        // i18n name
-        const jaName = skill.name;
-        const localName = this.$t(`skill.${jaName}`);
-        if (localName.startsWith('skill.')) {
-          newSkillNames[jaName] = '';
-        }
-        skill.name = localName && !localName.startsWith('skill.') ? localName : jaName;
+        this.localizeSkill(skill, undefined, newSkillNames);
       }
       if (Object.keys(newSkillNames).length > 0) {
         console.log(JSON.stringify(newSkillNames));
@@ -1108,6 +1096,25 @@ export default {
         return rarity;
       }
     },
+    localizeSkill(skill, wrapper, newSkillNames) {
+      const jaName = skill.name;
+      const localName = this.$t(`skill.${jaName}`);
+      if (localName.startsWith('skill.')) {
+        newSkillNames[jaName] = '';
+      }
+      skill.name = localName && !localName.startsWith('skill.') ? localName : jaName;
+
+      const tooltipKey = `tooltip.${skill.id}`;
+      if (this.$te(tooltipKey)) {
+        if (wrapper) {
+          wrapper.tooltip = this.$t(tooltipKey);
+        } else {
+          skill.tooltip = this.$t(tooltipKey);
+        }
+      }
+
+      return skill;
+    }
   },
 }
 </script>
--- src/i18n.js
+++ src/i18n.js
@@ -1,55 +1,53 @@
-import Vue from 'vue';
-import VueI18n from 'vue-i18n';
+import Vue from "vue";
+import VueI18n from "vue-i18n";
 
-import ja from './locales/ja.json';
-import zhTW from './locales/zhTW.json';
-import en from './locales/en.json';
-import ko from './locales/ko.json';
+import ja from "./locales/ja.json";
+import zhTW from "./locales/zhTW.json";
+import en from "./locales/en.json";
+import ko from "./locales/ko.json";
 
-import skillZhTW from './locales/skill-zhTW.json'
-import skillKo from './locales/skill-ko.json'
-import skillEn from './locales/skill-en.json'
+import skillZhTW from "./locales/skill-zhTW.json";
+import skillKo from "./locales/skill-ko.json";
+import skillEn from "./locales/skill-en.json";
+
+import tooltipKo from "./locales/tooltip-ko.json";
 
 Object.assign(zhTW, { skill: skillZhTW });
-Object.assign(ko, { skill: skillKo });
+Object.assign(ko, { skill: skillKo, tooltip: tooltipKo });
 Object.assign(en, { skill: skillEn });
 
-Vue.use(VueI18n)
+Vue.use(VueI18n);
 
-const lang_array = ['ja', 'zhTW', 'ko', 'en']
+const lang_array = ["ja", "zhTW", "ko", "en"];
 const messages = {
-	ja,
-	zhTW,
-	ko,
-	en,
+  ja,
+  zhTW,
+  ko,
+  en,
 };
 
-let lang
-let locale_lang
+let lang;
+let locale_lang;
 let locale;
 
-if (localStorage.getItem('lang') !== null) {
-	locale = localStorage.getItem('lang')
+if (localStorage.getItem("lang") !== null) {
+  locale = localStorage.getItem("lang");
 } else {
+  // get locale language
+  locale_lang = window.navigator.userLanguage || window.navigator.language;
+  lang = locale_lang.replace("-", "");
 
-	// get locale language
-	locale_lang = (window.navigator.userLanguage || window.navigator.language)
-	lang = locale_lang.replace('-', '')
-
-	// if locale language is not in setting, use japanese as default
-	if (lang_array.includes(lang)) {
-		locale = lang
-	} else {
-		locale = 'ja'
-	}
-
+  // if locale language is not in setting, use japanese as default
+  if (lang_array.includes(lang)) {
+    locale = lang;
+  } else {
+    locale = "ja";
+  }
 }
-
-
 
 const i18n = new VueI18n({
   locale,
   messages,
-})
+});
 
-export default i18n
+export default i18n;
